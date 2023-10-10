f3b3eb6b5db36787e6639c0ba713d28c10e14119
SegCode200 <ajayisegun2003@example.com>
2023-10-10 07:33:03 +0100

    second commit

--- src/pages/Allpages/Card.tsx
+++ src/pages/Allpages/Card.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ButtonProps from '../../components/reuse/ButtonProps'
 import styled from 'styled-components'
 import pics from "../../assets/1_d616899a-5e2f-4d62-a93e-8a324b137bc2.svg"
+import pics1 from "../../assets/1_e18ecc17-749e-404a-9582-11c75701d2e7.svg"
 
 const Card = () => {
   return (
@@ -17,7 +18,20 @@ const Card = () => {
                 </Left>
                 <Right>
                     <ImageHolder>
-                        <Image/>
+                        <Image src={pics1}/>
+                    </ImageHolder>
+                </Right>
+                </Holder>
+                <Holder style={{backgroundImage: `url(${pics})` }}>
+                <Left>
+                <Button>New</Button>
+                <Text>Planet Severless Drive for Javascript</Text>
+                <Text1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis excepturi sapiente accusamus alias! Nihil voluptates doloribus molestiae.</Text1>
+                <ButtonProps text='Check out the repo'/>
+                </Left>
+                <Right>
+                    <ImageHolder>
+                        <Image src={pics1}/>
                     </ImageHolder>
                 </Right>
                 </Holder>
@@ -28,11 +42,30 @@ const Card = () => {
 }
 
 export default Card
-const Image = styled.div``
-const ImageHolder = styled.div``
-const Right = styled.div``
-const Text1 = styled.div``
-const Text = styled.div``
+const Image = styled.img`
+border-radius: 20px;
+background-color: rgba(43, 43, 43, 0.7);
+`
+const ImageHolder = styled.div`
+border-radius: 20px;
+height: 100px;
+width: 100px;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: rgba(43, 43, 43, 0.7);
+padding: 3px;
+`
+const Right = styled.div`
+margin-right: 10px;
+`
+const Text1 = styled.div`
+color: #8a8a8a;
+margin: 20px 0;
+`
+const Text = styled.div`
+margin: 15px 0;
+`
 const Button = styled.div`
 width: 50px;
 height: 30px;
@@ -52,7 +85,9 @@ const Holder = styled.div`
 width: 550px;
 padding: 20px;
 height: 200px;
+display: flex;
 border-radius: 10px;
+margin: 0 10px;
 background-color: #121212;
 `
 const Main = styled.div`
